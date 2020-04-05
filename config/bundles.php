6ad63a6b628234dd1c4c1909eabd7aa59dd43188
pierodc/proyectoPrueba <piero@dicampo.com>
2020-04-05 01:08:59 -0400

    1.

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
---
+++ src/Entity/Producto.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductoRepository")
+ */
+class Producto
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Nombre;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $Descripcion;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $Existencia;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->Nombre;
+    }
+
+    public function setNombre(string $Nombre): self
+    {
+        $this->Nombre = $Nombre;
+
+        return $this;
+    }
+
+    public function getDescripcion(): ?string
+    {
+        return $this->Descripcion;
+    }
+
+    public function setDescripcion(?string $Descripcion): self
+    {
+        $this->Descripcion = $Descripcion;
+
+        return $this;
+    }
+
+    public function getExistencia(): ?int
+    {
+        return $this->Existencia;
+    }
+
+    public function setExistencia(int $Existencia): self
+    {
+        $this->Existencia = $Existencia;
+
+        return $this;
+    }
+}
